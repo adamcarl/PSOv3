968aeff4aee2533dd4df8543f2bfaeb3ba1524c8
Adam Medina <adamcarlmedina@gmail.com>
2017-05-09 11:37:39 +0800

    With import and print

--- app/src/main/java/com/example/sydney/psov3/Cashier.java
+++ app/src/main/java/com/example/sydney/psov3/Cashier.java
@@ -3,6 +3,7 @@ package com.example.sydney.psov3;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -11,14 +12,19 @@ import android.os.Message;
 import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.ExpandedMenuView;
+import android.text.Editable;
 import android.text.InputType;
+import android.text.TextWatcher;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TabHost;
 import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,6 +32,7 @@ import com.hdx.lib.serial.SerialParam;
 import com.hdx.lib.serial.SerialPortOperaion;
 
 import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -47,7 +54,7 @@ public class Cashier extends AppCompatActivity {
     DB_Data db_data;
     ContentValues cv;
     ArrayList<String> items;
-    EditText searchText;
+    EditText searchText,txt_cash;
     String itemnameCol;
     double itempriceCol;
     int quantityCount = 0;
@@ -63,6 +70,8 @@ public class Cashier extends AppCompatActivity {
     SQLiteDatabase dbReader;
     SQLiteDatabase dbWriter;
     TextView lbl_price;
+    TabHost host;
+    AnimatedTabHostListener getTabHost;
 
     protected void onCreate(Bundle savedInstanceState) {
         db_data = new DB_Data(this);
@@ -79,17 +88,55 @@ public class Cashier extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cashier);
         searchText =(EditText) findViewById(R.id.txt_cashier_search);
+        txt_cash = (EditText)findViewById(R.id.txt_cash);
         layout = (RelativeLayout)findViewById(R.id.rl_cashier);
+        host = (TabHost)findViewById(R.id.tabHost);
+///        getTabHost = new AnimatedTabHostListener(this,host);
+        host.setup();
+
+        //Tab 1
+
+        TabHost.TabSpec spec = host.newTabSpec("Invoice");
+
+        spec.setContent(R.id.tab1);
+
+        spec.setIndicator("Invoice");
+
+        host.addTab(spec);
+
+
+
+        //Tab 2
+
+        spec = host.newTabSpec("Payment");
+
+        spec.setContent(R.id.tab2);
+
+        spec.setIndicator("Paayment");
+
+        host.addTab(spec);
+
+
+
+        //Tab 3
+
+        spec = host.newTabSpec("Shift");
+
+        spec.setContent(R.id.tab3);
+
+        spec.setIndicator("Shift");
+
+        host.addTab(spec);
+
         Calendar c = Calendar.getInstance();
         SimpleDateFormat dateformat = new SimpleDateFormat("MM.dd.yyyy");
         String dateformatted = dateformat.format(c.getTime());
         SimpleDateFormat sdf = new SimpleDateFormat("hh:mm a");
         String currentTime = sdf.format(new Date());
-        lbl_price=(TextView)findViewById(R.id.lbl_price);
         dbWriter.execSQL("INSERT INTO sessions(time,date,username) VALUES(time('now'),date('now'),'"+firstname+"') ");
         items = new ArrayList<String>();
-        items.add("Name");
         items.add("Quantity");
+        items.add("Name");
         items.add("Price");
         products.add("     ABZTRAK INC CONVENIENCE STORE");
         products.add("2nd Floor, #670,");
@@ -98,61 +145,111 @@ public class Cashier extends AppCompatActivity {
         products.add("             Cash Invoice");
         products.add("Date: \t "+dateformatted+" \t "+currentTime+"");
         products.add("--------------------------------------");
-        products.add("Name          Quantity           Price");
+        products.add("Quantity          Name           Price");
         GridView grid = (GridView) findViewById(R.id.grd_sell);
         grid.setAdapter(new ArrayAdapter<String>(this,R.layout.single_cell,items));
+
+        //host.setOnTabChangedListener(new AnimatedTabHostListener(this,host));
     }
-    public void search(View view) {
-        int code = Integer.parseInt(searchText.getText().toString());
-        final String[] itemcode = {Integer.toString(code)};
-        String[] WHERE = {ID_PRODUCT, NAME_PRODUCT, QUAN_PRODUCT, PRICE_PRODUCT};
-        cursor = dbReader.query(TABLE_NAME_PRODUCT,WHERE,ID_PRODUCT + " LIKE ?",itemcode,null,null,null);
-        cursor.moveToFirst();
-        int rows = cursor.getCount();
-        if(rows>=1) {
-            // 1. Instantiate an AlertDialog.Builder with its constructor
-            final EditText dialogText = new EditText(this);
-            dialogText.setInputType(InputType.TYPE_CLASS_NUMBER);
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("Enter Quantity");
-            builder.setView(dialogText);
-            builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int whichButton)
-                {
-                    dialogVar = Integer.parseInt(dialogText.getText().toString());
-                    itempriceCol = cursor.getInt(cursor.getColumnIndex(PRICE_PRODUCT));
-                    itemnameCol = cursor.getString(cursor.getColumnIndex(NAME_PRODUCT));
-                    itemcodeCol = cursor.getInt(cursor.getColumnIndex(ID_PRODUCT));
-                    itemQuantityList.add(dialogVar);
-                    itemPriceList.add( itempriceCol);
-                    itemNameList.add(itemnameCol);
-                    itemCodeList.add(itemcodeCol);
-                    itempriceCol = dialogVar * itempriceCol;
-                    ArrayList<Double>total = new ArrayList<Double>();
-                    total.add(itempriceCol);
-                    items.add("" + itemnameCol + "");
-                    items.add(""+dialogVar+"");
-                    items.add("" + itempriceCol + "");
-                    cursor.close();
-                    layout.invalidate();
-                    layout.requestLayout();
-                    for(int x =0;x<total.size();x++) {
-                        totalPrice = totalPrice += total.get(x);
-                        quantityCount++;
-                    }
-                    DecimalFormat format = new DecimalFormat("#.##");
-                    vattable = totalPrice / 1.12;
-                    vat = vattable * 0.12;
-                    vattable2 =  Double.valueOf(format.format(vattable));
-                    vat2 = Double.valueOf(format.format(vat));
-                    products.add(""+itemnameCol+"\t "+dialogVar+"\t \t \t"+itempriceCol+"");
+    public void priceClick(View view) {
+        txt_cash.requestFocus();
+        txt_cash.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void afterTextChanged(Editable arg0) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            private String current = "";
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().equals(current)) {
+                    txt_cash.removeTextChangedListener(this);
+
+                    String cleanString = s.toString().replaceAll("[P,.]", "");
+
+                    double parsed = Double.parseDouble(cleanString);
+                    String formatted = NumberFormat.getCurrencyInstance().format((parsed / 100));
+                    formatted = formatted.replace('$','P');
+                    current = formatted;
+                    txt_cash.setText(formatted);
+                    txt_cash.setSelection(formatted.length());
+
+                    txt_cash.addTextChangedListener(this);
                 }
-            });
-            Dialog dialog = builder.create();
-            dialog.show();
+            }
+        });
+    }
+
+    public void search(View view) {
+        try {
+            int code = Integer.parseInt(searchText.getText().toString());
+            final String[] itemcode = {Integer.toString(code)};
+            String[] WHERE = {ID_PRODUCT, NAME_PRODUCT, QUAN_PRODUCT, PRICE_PRODUCT};
+            cursor = dbReader.query(TABLE_NAME_PRODUCT, WHERE, ID_PRODUCT + " LIKE ?", itemcode, null, null, null);
+            cursor.moveToFirst();
+            int rows = cursor.getCount();
+            if (rows >= 1) {
+                // 1. Instantiate an AlertDialog.Builder with its constructor
+                final EditText dialogText = new EditText(this);
+                dialogText.setInputType(InputType.TYPE_CLASS_NUMBER);
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Enter Quantity");
+                builder.setView(dialogText);
+                searchText.setText("");
+                builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        try {
+                                if (dialogText.getText().toString().equals("")){
+                                    Toast.makeText(getApplicationContext(), "Please Enter Quantity.", Toast.LENGTH_SHORT).show();
+                                }
+                                else {
+                                    dialogVar = Integer.parseInt(dialogText.getText().toString());
+                                    itempriceCol = cursor.getInt(cursor.getColumnIndex(PRICE_PRODUCT));
+                                    itemnameCol = cursor.getString(cursor.getColumnIndex(NAME_PRODUCT));
+                                    itemcodeCol = cursor.getInt(cursor.getColumnIndex(ID_PRODUCT));
+                                    itemQuantityList.add(dialogVar);
+                                    itemPriceList.add(itempriceCol);
+                                    itemNameList.add(itemnameCol);
+                                    itemCodeList.add(itemcodeCol);
+                                    itempriceCol = dialogVar * itempriceCol;
+                                    ArrayList<Double> total = new ArrayList<Double>();
+                                    total.add(itempriceCol);
+                                    items.add("" + dialogVar + "");
+                                    items.add("" + itemnameCol + "");
+                                    items.add("" + itempriceCol + "");
+                                    cursor.close();
+                                    layout.invalidate();
+                                    layout.requestLayout();
+                                    for (int x = 0; x < total.size(); x++) {
+                                        totalPrice = totalPrice += total.get(x);
+                                        quantityCount++;
+                                    }
+                                    DecimalFormat format = new DecimalFormat("#.##");
+                                    vattable = totalPrice / 1.12;
+                                    vat = vattable * 0.12;
+                                    vattable2 = Double.valueOf(format.format(vattable));
+                                    vat2 = Double.valueOf(format.format(vat));
+                                    products.add("" + itemnameCol + "\t " + dialogVar + "\t \t \t" + itempriceCol + "");
+                                }
+                        }catch (Exception ex){
+                            ex.printStackTrace();
+                        }
+                    }
+                });
+                Dialog dialog = builder.create();
+                dialog.show();
+            } else Toast.makeText(this, "Product cant be found", Toast.LENGTH_LONG).show();
         }
-        else Toast.makeText(this, "Product cant be found", Toast.LENGTH_LONG).show();
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
     }
     public void print(View view) {
 //            final EditText dialogText = new EditText(Cashier.this);
@@ -251,4 +348,5 @@ public class Cashier extends AppCompatActivity {
             }
         }
     }
+    
 }
