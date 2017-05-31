6231c55c5df969dbb689cd729fd9754f176f08b6
Adam Medina <adamcarlmedina@gmail.com>
2017-05-31 09:19:16 +0800

    May 31 , 2017 9:20 Tapos na ActivityLogin

--- app/src/main/java/com/example/sydney/psov3/MainActivity.java
+++ app/src/main/java/com/example/sydney/psov3/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.sydney.psov3;
 
-import android.app.Fragment;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -37,13 +34,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         init();
 
-        //For ActivityLogin
-        tv_signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                layout_signup.setVisibility(View.VISIBLE);
-            }
-        });
+//        //For ActivityLogin
+//        tv_signup.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                layout_signup.setVisibility(View.VISIBLE);
+//            }
+//        });
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -80,42 +77,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //For SignUp
-        btn_cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                layout_signup.setVisibility(View.GONE);
-                et_regFname.setText("");
-                et_regLname.setText("");
-                et_regUsernum.setText("");
-                et_regPass.setText("");
-                spn_regPosition.setSelection(0);
-                et_usernum.setText("");
-                et_pass.setText("");
-            }
-        });
-        btn_register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String mfname = et_regFname.getText().toString().trim();
-                String mlname = et_regLname.getText().toString().trim();
-                String mnum = et_regUsernum.getText().toString().trim();
-                String mpass = et_regPass.getText().toString().trim();
-                String mpos = spn_regPosition.getSelectedItem().toString();
-                try {
-                    db_data.addCashier(mfname,mlname,mnum,mpass,mpos);
-                    et_regFname.setText("");
-                    et_regLname.setText("");
-                    et_regUsernum.setText("");
-                    et_regPass.setText("");
-                    spn_regPosition.setSelection(0);
-                    Toast.makeText(MainActivity.this, "Registration successful!", Toast.LENGTH_SHORT).show();
-                }
-                catch (Exception e){
-                    e.printStackTrace();
-                }
-            }
-        });
+//        //For SignUp
+//        btn_cancel.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                layout_signup.setVisibility(View.GONE);
+//                et_regFname.setText("");
+//                et_regLname.setText("");
+//                et_regUsernum.setText("");
+//                et_regPass.setText("");
+//                spn_regPosition.setSelection(0);
+//                et_usernum.setText("");
+//                et_pass.setText("");
+//            }
+//        });
+//        btn_register.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String mfname = et_regFname.getText().toString().trim();
+//                String mlname = et_regLname.getText().toString().trim();
+//                String mnum = et_regUsernum.getText().toString().trim();
+//                String mpass = et_regPass.getText().toString().trim();
+//                String mpos = spn_regPosition.getSelectedItem().toString();
+//                try {
+//                    db_data.addCashier(mfname,mlname,mnum,mpass,mpos);
+//                    et_regFname.setText("");
+//                    et_regLname.setText("");
+//                    et_regUsernum.setText("");
+//                    et_regPass.setText("");
+//                    spn_regPosition.setSelection(0);
+//                    Toast.makeText(MainActivity.this, "Registration successful!", Toast.LENGTH_SHORT).show();
+//                }
+//                catch (Exception e){
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
     }
 
     //Initialization
@@ -124,32 +121,32 @@ public class MainActivity extends AppCompatActivity {
         db_data = new DB_Data(this);
         
         //For LogIn
-        btn_login=(Button)findViewById(R.id.btn_login);
-        et_usernum=(EditText)findViewById(R.id.et_usernum);
-        et_pass=(EditText)findViewById(R.id.et_pass);
-        tv_signup=(TextView)findViewById(R.id.tv_signup);
+        btn_login=(Button)findViewById(R.id.btnLogin);
+        et_usernum=(EditText)findViewById(R.id.etUsernum);
+        et_pass=(EditText)findViewById(R.id.etPassword);
+        tv_signup=(TextView)findViewById(R.id.txtSignup);
         
-        //For SignUp
-        layout_signup=(LinearLayout)findViewById(R.id.ll_signup);
-        btn_cancel=(Button)findViewById(R.id.btn_cancel);
-        btn_register=(Button)findViewById(R.id.btn_signup);
-        
-        //For Register
-        et_regFname=(EditText)findViewById(R.id.et_regFname);
-        et_regLname=(EditText)findViewById(R.id.et_regLname);
-        et_regUsernum=(EditText)findViewById(R.id.et_regUsernum);
-        et_regPass=(EditText)findViewById(R.id.et_regPass);
-        spn_regPosition=(Spinner)findViewById(R.id.spn_regPosition);        
+//        //For SignUp
+//        layout_signup=(LinearLayout)findViewById(R.id.fragment_signup);
+//        btn_cancel=(Button)findViewById(R.id.btnCancel);
+//        btn_register=(Button)findViewById(R.id.btnSignup);
+//
+//        //For Register
+//        et_regFname=(EditText)findViewById(R.id.et_regFname);
+//        et_regLname=(EditText)findViewById(R.id.et_regLname);
+//        et_regUsernum=(EditText)findViewById(R.id.et_regUsernum);
+//        et_regPass=(EditText)findViewById(R.id.et_regPass);
+//        spn_regPosition=(Spinner)findViewById(R.id.spn_regPosition);
     }
     //BackButton Holder
     public void onBackPressed(){
 
     }
-    //Fragments
-    public static class FragmentSignUp extends Fragment {
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_signup, container, false);
-        }
-    }
+//    //Fragments
+//    public static class FragmentSignUp extends Fragment {
+//        @Override
+//        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//            return inflater.inflate(R.layout.fragment_signup, container, false);
+//        }
+//    }
 }
